1c6776da8eb369b87dfdeda571ddc9ef880a0561
Roger Reyes <roger.reyes@inzpiral.com>
2013-06-06 20:36:33 -0400

    Creacion del rpoyecto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 myaccount
-=========
+========
+Herramienta para el control de cuentas en el debe y el haber de los pagos diarios realizados para personas que viven juntas en una misma casa.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myaccount::Application.config.secret_token = '7e1aeef38a040f11d2bcb1aaf272b192d3ed86dfbf59ef1f2b955395c552d9cda67a94499a30aa162153f3f4e3f68257119d79d0ae03608527fae0735922e22d'
